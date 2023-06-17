de270c266d86cfcd88e23422e282d00c08bd41c4
Vikas Chaudhary <vikassixfeet@gmail.com>
2023-06-17 17:34:34 +0530

    Classes and object introduction

---
+++ OOPS INTRODUCTION/Classes_Objects.java
@@ -0,0 +1,52 @@
+
+//class- data +functions together
+//reference of an object
+//instance of an class.
+//instance == object
+//reference -stack - points (this parameter by default)to ab object
+//data members and methods -class
+//(verb + actions )
+// "new" operators works on only class and give address of the instance/object to us.
+
+
+
+
+public class Classes_Objects {
+    private static class person{
+        String name;
+        int age;
+        void sayHi(){
+            if(this.age>30){
+                System.out.println("hello from  "+this.name);
+
+            }else{
+                System.out.println("HI from  "+this.name);
+
+            }
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        person p1;
+        p1=new person();
+        p1.age=24;
+        p1.name="vikas";
+        p1.sayHi();
+
+
+
+        person p2;
+        p2=new person();
+        p2.age=30;
+        p2.name="rohan";
+        p2.sayHi();
+
+
+
+    }
+
+}
+
